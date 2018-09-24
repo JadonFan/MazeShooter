97a505e4ae6904bcec983e6c443d8caa9ae12223
Jadon Fan <35983083+JadonFan@users.noreply.github.com>
2018-09-24 19:50:05 -0400

    3D Graphics Added

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from cx_Freeze import setup, Executable
 ## os.environ["TCL_LIBRARY"] = ...
 
 base = None
-if sys.platform == "win32":  
+if sys.platform == "win32":
     base = "Win32GUI"
 
 extra_packages = ["pygame", "numpy"]
@@ -12,7 +12,7 @@ extra_modules = ["numpy.core._methods", "numpy.lib.format"]
 extra_files = ["arrowup.png", "arrowright.png", "arrowleft.png", "arrowdown.png", "person.png", "audiosign.jpg", "mazegamemusic.wav", "uwlogo.png"]
 
 setup(name = "mazeshooter" ,
-      author = "Jadon Fan",
+	  author = "Jadon Fan",
       version = "1.0" ,
       description = "Maze Shooting Game" ,
       options = {"build_exe": {"includes": extra_modules, "include_files": extra_files}},
